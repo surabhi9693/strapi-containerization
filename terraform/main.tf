4751cf3ffe6f9ce0ebdd88636a1f64c60187adbb
surabhi9693 <127684006+surabhi9693@users.noreply.github.com>
2024-06-24 20:48:22 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -93,10 +93,10 @@ resource "null_resource" "example" {
       "sudo apt-get install apt-transport-https ca-certificates curl software-properties-common",
       "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
       "sudo add-apt-repository deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable --yes",
-      "sudo apt-get update",
-      "sudo apt-get install docker-ce",
-      "sudo systemctl status docker",
-      "sudo docker build -t my-strapi-app .",
+      "sudo apt-get update --yes ",
+      "sudo apt-get install docker-ce --yes",
+      "sudo systemctl status docker --yes",
+      "sudo docker build -t my-strapi-app . --yes ",
       "sudo docker run -p 1337:1337 my-strapi-app"
     ]
 }
