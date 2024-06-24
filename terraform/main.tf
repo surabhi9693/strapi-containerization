d376f9b83fd2e33bc8adfd0d0345e58b46b537d6
surabhi9693 <127684006+surabhi9693@users.noreply.github.com>
2024-06-24 21:08:37 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -97,6 +97,7 @@ resource "null_resource" "example" {
       "sudo apt-get install docker-ce --yes",
       "sudo systemctl status docker --yes",
       "git clone https://github.com/surabhi9693/strapi-containerization.git",
+      "cd strapi-containerization/"
       "sudo docker build -t my-strapi-app .",
       "sudo docker run -p 1337:1337 my-strapi-app"
     ]
