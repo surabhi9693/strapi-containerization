9cedffbe616257393740ebd99bdccb6dad385d34
surabhi9693 <127684006+surabhi9693@users.noreply.github.com>
2024-06-24 20:44:04 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -92,7 +92,7 @@ resource "null_resource" "example" {
       "sudo apt-get update",
       "sudo apt-get install apt-transport-https ca-certificates curl software-properties-common",
       "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
-      "sudo add-apt-repository deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable",
+      "sudo add-apt-repository deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable --yes",
       "sudo apt-get update",
       "sudo apt-get install docker-ce",
       "sudo systemctl status docker",
