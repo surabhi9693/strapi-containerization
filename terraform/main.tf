bf088043681671bd48b7d52510b17fbaf1b5ea2d
surabhi9693 <127684006+surabhi9693@users.noreply.github.com>
2024-06-24 20:13:24 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -90,16 +90,15 @@ resource "null_resource" "example" {
     }
     inline = [
       #!/bin/bash
-      sudo apt update
-      sudo apt install apt-transport-https ca-certificates curl software-properties-common
+      sudo apt-get update
+      sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
       curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
       sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-      sudo apt update
-      sudo apt install docker-ce
+      sudo apt-get update
+      sudo apt-get install docker-ce
       sudo systemctl status docker
-      sudo usermod -aG docker ${USER}
-      docker build -t my-strapi-app .
-      docker run -p 1337:1337 my-strapi-app
+      sudo docker build -t my-strapi-app .
+      sudo docker run -p 1337:1337 my-strapi-app
     ]
 }
  depends_on = [
